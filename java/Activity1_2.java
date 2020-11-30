7be1b894b1bc81ff8e7ca0938a9bab35c9731516
ninsamue <37399162+ninsamue@users.noreply.github.com>
2020-11-30 22:12:53 +0530

    Add files via upload

---
+++ java/Activity1_2.java
@@ -0,0 +1,33 @@
+package JavaActivity2;
+
+public class Activity1_2 {
+
+	public static void main(String[] args) {
+		int[] arr = {10, 77, 10, 54, -11, 10};
+		
+		if(result(arr)==true) {
+			System.out.println("The sum of all elements in array is 30");
+		}
+		else
+		{
+			System.out.println("The sum of all elements in the array is not 30");
+		}
+		
+	}
+	
+	public static boolean result(int[] arr) {
+		int sum=0;
+		for(int i:arr)
+		{
+			if(i==10)
+				sum=sum+i;
+		}
+		
+		//check if the sum of all the 10's in the array is exactly 30
+		if(sum==30)
+			return true;
+		else
+			return false;
+		
+	}
+}
